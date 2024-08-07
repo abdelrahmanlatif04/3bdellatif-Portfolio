ff05fe377d5588a420e6d4ba95f4d464727c8cf9
abdelrahmanlatif04 <abdelrahmanlatif04@gmail.com>
2024-08-07 06:31:07 +0300

    mod

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div
-    class="smooth h-[300vh] flex flex-col pt-10 md:pt-16"
-    :class="theme == 'dark' ? 'bg-secondary-dark' : 'bg-secondary-light'"
+    class="smooth flex flex-col font-mono h-[300vh] md:py-0 py-5"
+    :class="
+      theme == 'dark'
+        ? 'bg-secondary-dark text-white'
+        : 'bg-secondary-light  text-[#fa0]' //needs text color here
+    "
   >
     <NavBar :theme="theme" @changeTheme="changeTheme" />
+    <!-- <section
+      id="home"
+      class="flex flex-col sm:flex-row gap-8 text-center items-center sm:text-3xl text-sm sm:text-start"
+    >
+    <div class="w-1/2">
+      <img
+        src="./assets/avatar.jpg"
+        class="rounded-full border-4 border-white"
+      />
+    </div>
+      <div class="header flex flex-col justify-center w-1/2">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident id
+        maiores expedita repudiandae nihil, nobis dolore quae atque perferendis
+        saepe minima, asperiores repellat, adipisci quasi harum officia unde
+        quod temporibus! Labore, facere quae! Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Aspernatur, nihil!
+      </div>
+    </section> -->
 
-    <section id="home">
-      <div class="title">Home</div>
+    <section id="header">
+      <div class="title">Header</div>
     </section>
     <section id="about">
       <div class="title">About</div>
@@ -14,8 +36,32 @@
     <section id="portfolio">
       <div class="title">Portfolio</div>
     </section>
-    <section id="contact">
-      <div class="title">Contact</div>
+    <section id="contact" class="pb-2">
+      <div class="title">Contact Me :)</div>
+      <div class="flex gap-3 my-6 sm:text-xl tracking-tighter ml-2 flex-wrap">
+        <span>
+          you can visit my Social Hub
+          <a
+            target="_blank"
+            href="https://3bdellatiif-social-links.vercel.app/"
+            class="underline underline-offset-2 hover:opacity-60 hover:underline-offset-4 text-[#fa0] smooth"
+            >here</a
+          >
+        </span>
+        or contact me directly using these links
+      </div>
+      <ul
+        class="flex justify-around gap-y-10 gap-5 mt-6 flex-wrap max-w-[800px] mx-auto text-lg"
+      >
+        <li
+          v-for="item in contacts"
+          :key="item"
+          class="h-5 flex justify-center items-center gap-3 cursor-pointer hover:opacity-55 smooth"
+        >
+          <img :src="item.img" class="h-full invert" />
+          {{ item.title }}
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -25,16 +71,46 @@
   @apply transition-all duration-300;
 }
 section {
-  @apply p-10;
+  @apply p-5 border-y;
+}
+.title {
+  @apply text-3xl font-semibold;
 }
 </style>
 
 <script>
 import NavBar from "../src/components/Nav.vue";
+import fbImg from "./assets/facebook.svg";
+import emailImg from "./assets/mail.png";
+import githubImg from "./assets/github.png";
+import linkedInImg from "./assets/linkedin.png";
+
 export default {
   data() {
     return {
       theme: "dark",
+      contacts: [
+        {
+          title: "Facebook",
+          img: fbImg,
+          dist: "https://www.facebook.com/profile.php?id=100003984241851&mibextid=ZbWKwL",
+        },
+        {
+          title: "LinkedIn",
+          img: linkedInImg,
+          dist: "https://www.linkedin.com/in/abdelrahmanlatif/",
+        },
+        {
+          title: "Email",
+          img: emailImg,
+          dist: "mailto:abdelrahmanlatif04@gmail.com",
+        },
+        {
+          title: "Github",
+          img: githubImg,
+          dist: "https://github.com/abdelrahmanlatif04",
+        },
+      ],
     };
   },
   methods: {
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,41 +1,39 @@
 <template>
   <div
     :class="`${theme == 'dark' ? 'bg-primary-dark' : 'bg-primary-light'}     
-    ${state ? '-translate-x-full' : 'translate-x-0'}`"
-    class="w-[45%] h-full fixed md:absolute smooth md:translate-x-0 md:w-full left-0 top-0 md:h-fit"
+    ${pos}`"
+    class="w-[45%] h-full fixed md:relative smooth md:translate-x-0 md:w-full left-0 top-0 md:h-fit z-50"
   >
-    <div
+    <!-- <span
       class="theme-btn absolute left-3 top-3 p-1 flex items-center h-6 aspect-[2] rounded-xl cursor-pointer bg-sky-600"
       @click="this.$emit('changeTheme')"
     >
       <div
         class="aspect-square smooth h-4 relative rounded-full bg-orange-500 before:aspect-square before:absolute before:smooth before:rounded-full"
         :class="
-          theme == 'light'
-            ? 'left-0 bg-orange-400 before:bg-orange-600 before:w-2/3 before:right-1/2 before:top-1/2 before:translate-x-1/2 before:-translate-y-1/2'
-            : 'left-full -translate-x-full bg-white before:bg-sky-600 before:w-4/5 before:right-0 before:top-0'
+          theme == 'light'?
+          'left-0 bg-orange-400 before:bg-orange-600 before:w-2/3 before:right-1/2 before:top-1/2 before:translate-x-1/2 before:-translate-y-1/2':            'left-full -translate-x-full bg-white before:bg-sky-600 before:w-4/5 before:right-0 before:top-0'
         "
       ></div>
-    </div>
+    </span> -->
 
-    <span
-      @click="toggle()"
-      class="md:hidden absolute top-1/2 right-0 z-10 -translate-y-1/2 translate-x-1/2 w-12 h-32 rounded-full cursor-pointer smooth"
-      :class="theme == 'dark' ? 'bg-primary-dark' : 'bg-primary-light'"
-    >
-      <img
-        class="absolute top-1/2 -translate-y-1/2 w-1/2 right-[1px]"
-        :class="`${state ? 'rotate-180 ' : 'rotate-0'} ${
-          theme == 'dark' ? 'invert-[60%]' : 'invert-[30%]'
-        }`"
-        src="../assets/arrow.png"
-        alt=""
-      />
-    </span>
     <ul
-      class="flex flex-col justify-center items-center w-full h-full gap-8 text-2xl font-semibold tracking-widest md:flex-row p-3 md:text-lg md:justify-evenly"
+      class="flex flex-col justify-center items-center w-full h-full gap-4 text-2xl font-semibold tracking-widest md:flex-row p-3 md:text-lg md:justify-evenly"
     >
-      <li v-for="link in links" :key="link">
+      <span
+        @click="toggle()"
+        class="md:hidden absolute top-1/2 right-0 -z-10 -translate-y-1/2 translate-x-1/2 w-10 h-24  rounded-full cursor-pointer smooth"
+        :class="theme == 'dark' ? 'bg-primary-dark' : 'bg-primary-light'"
+      >
+        <img
+          class="absolute top-1/2 -translate-y-1/2 w-1/2 right-[1px]"
+          :class="`${state ? 'rotate-180' : 'rotate-0'} ${
+            theme == 'dark' ? 'invert-[60%]' : 'invert-[30%]'
+          }`"
+          src="../assets/arrow.png"
+        />
+      </span>
+      <li v-for="link in links" :key="link" class="w-full flex justify-center">
         <a
           class="smooth hover:opacity-25"
           :class="theme == 'dark' ? 'text-white' : 'text-[#fa0]'"
@@ -51,7 +49,7 @@
 export default {
   data() {
     return {
-      state: false,
+      state: true,
       links: [
         { title: "Home", href: "#home" },
         { title: "About", href: "#about" },
@@ -64,32 +62,13 @@ export default {
     toggle() {
       this.state = !this.state;
     },
-    asd() {
-      let lastScrollTop = 0;
-      const navbar = document.querySelector(".navbar");
-
-      window.addEventListener("scroll", function () {
-        let scrollTop =
-          window.pageYOffset || document.documentElement.scrollTop;
-
-        if (scrollTop > lastScrollTop) {
-          // Downscroll
-          navbar.style.top = "-60px";
-        } else {
-          // Upscroll
-          navbar.style.top = "0";
-        }
-
-        lastScrollTop = scrollTop;
-      });
-    },
   },
   computed: {
     pos() {
       if (this.state) {
         return "-translate-x-full";
       } else {
-        return "-translate-x-0";
+        return "translate-x-0";
       }
     },
   },
